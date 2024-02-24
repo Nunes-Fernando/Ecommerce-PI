4d9cdf0e2489ff7e687b5fd532479d214ce0a1ed
Fernando Nunes <fn4586@gmail.com>
2024-02-24 18:49:06 -0300

    Tela para listar criada

--- src/main/java/com/ecommerce/ecommerceInimigosCodigo/PrincipalController.java
+++ src/main/java/com/ecommerce/ecommerceInimigosCodigo/PrincipalController.java
@@ -4,11 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class LoginController {
-    
-    @GetMapping("/login")
-    public String showLoginPage() {
-        return "login"; 
-    }
+public class PrincipalController {
 
+    @GetMapping("/principal")
+    public String showPrincipalPage() {
+        return "principal";
+    }
 }
+
