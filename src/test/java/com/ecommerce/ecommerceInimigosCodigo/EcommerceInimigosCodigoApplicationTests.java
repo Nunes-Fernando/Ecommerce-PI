d4f4a7e66e4a1604344986c96f29da67b614b609
Fernando Nunes <fn4586@gmail.com>
2024-02-24 15:19:31 -0300

    Desenvolvendo as primeiras telas

---
+++ src/test/java/com/ecommerce/ecommerceInimigosCodigo/EcommerceInimigosCodigoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.ecommerceInimigosCodigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceInimigosCodigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
